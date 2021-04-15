0ddf8a479e3f4ac3000ad8ae7c006d623a8b5781
Young <young20050727@gmail.com>
2021-04-15 15:58:38 +0800

    還沒做完啦哈哈

--- 20200415.cpp
+++ 20200415.cpp
@@ -195,42 +195,82 @@ int main(){
     for(int i=0;i<3;i++){
         s=1;
         cout << "====================== Class " << i+1 << " scores table ==============================================\n";
-        cout << "ID\t" << setw(15) << "Chinese" << setw(15) << "English" << setw(15)<< "Math" << setw(15) << "Sum" << "\tAVE\n";
+        cout << "ID\t" << setw(15) << "Chinese" << setw(15) << "English" << setw(15)<< "Math" << setw(15) << "Sum" << setw(12) << "\tAVE\n";
         for(int j=0;j<30;j++){
-            cout << s << "\t" << setw(15) << score[i][j][0] << setw(15) << score[i][j][1] << setw(15) << score[i][j][2]  << setw(15) << sum[i][j]  << "\t" << fixed << setprecision(2) <<ave[i][j] << "\n";
+            cout << s << "\t" << setw(15) << score[i][j][0] << setw(15) << score[i][j][1] << setw(15) << score[i][j][2]  << setw(15) << sum[i][j]  << setw(12) << fixed << setprecision(2)  << "\t" <<ave[i][j] << "\n";
             s++;
         }
     }
     cout << "==========================================================================================\n";
 
     cout << "Choice class 1 - 3 , 4 for all , C ==> Chinese , E ==> English , M ==> Math , 0 to eixt >>";
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    
+
+    int n;
+    cin >> n;
+    switch (n){
+
+    case 1:
+        cout << "===================================  Class " << i+1 << " =========================================\n";
+        s=1;
+        for(int j=0;j<30;j++){
+            cout << "Student ID : ";
+            cout << s << "\n";
+            cout << "Chinese ==> [" << score[i][j][0] << "] " << "\n";
+            cout << "English ==> [" << score[i][j][1] << "] " << "\n";
+            cout << "Math    ==> [" << score[i][j][2] << "] " << "\n";
+            s++;
+        }
+        break;
+    case 2:
+        cout << "===================================  Class " << i+1 << " =========================================\n";
+        s=2;
+        for(int j=0;j<30;j++){
+            cout << "Student ID : ";
+            cout << s << "\n";
+            cout << "Chinese ==> [" << score[i][j][0] << "] " << "\n";
+            cout << "English ==> [" << score[i][j][1] << "] " << "\n";
+            cout << "Math    ==> [" << score[i][j][2] << "] " << "\n";
+            s++;
+        }
+        break;
+    case 3:
+        cout << "===================================  Class " << i+1 << " =========================================\n";
+        s=3;
+        for(int j=0;j<30;j++){
+            cout << "Student ID : ";
+            cout << s << "\n";
+            cout << "Chinese ==> [" << score[i][j][0] << "] " << "\n";
+            cout << "English ==> [" << score[i][j][1] << "] " << "\n";
+            cout << "Math    ==> [" << score[i][j][2] << "] " << "\n";
+            s++;
+        }
+        break;
+        case 4:
+        for(int i=0;i<3;i++){
+            cout << "===================================  Class " << i+1 << " =========================================\n";
+            s=1;
+            for(int j=0;j<30;j++){
+                cout << "Student ID : ";
+                cout << s << "\n";
+                cout << "Chinese ==> [" << score[i][j][0] << "] " << "\n";
+                cout << "English ==> [" << score[i][j][1] << "] " << "\n";
+                cout << "Math    ==> [" << score[i][j][2] << "] " << "\n";
+                s++;
+            }
+        }
+    default:
+        
+        break;
     }
-    /*
-    char c;
-    while(1){
-        cin >> c;
-        if(c=='0') break;
-        else if(c=='1'){
-            
-        }
-        else if(c=='2'){
-            
-        }
-        else if(c=='3'){
-            
-        }
-        else if(c=='4'){
-            
-        }
-        else if(c=='C'){
-            
-        }
-        else if(c=='E'){
-            
-        }
-        else if(c=='M'){
-            
-        }
-    */
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
     return 0;
 }
