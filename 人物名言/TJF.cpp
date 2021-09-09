890ecea2f809fbef1bee3398d50af0bf2b2b857f
Young-TW <young20050727@gmail.com>
2021-09-10 00:22:58 +0800

    Create TJF.cpp

---
+++ 人物名言/TJF.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <system>
+using namespace std;
+
+int main() {
+  cout << "My name is yourfather. \n";
+  cout << "I am goder \n.";
+  system("pause");
+  return 0;
+}
