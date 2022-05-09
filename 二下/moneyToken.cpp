a8647d218503d5c6b38f0690a8f068c26eb77b72
Young-TW <young20050727@gmail.com>
2022-05-09 11:19:29 +0800

    moneytoken.cpp

---
+++ 二下/moneyToken.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+int main() {
+    int coin[4]= {1,5,10,50};
+    int m,n;
+    scanf("%d",&m); // 測資數量
+    for (int i=0; i<m; i++) {
+        scanf("%d", &n); // 輸入錢的數字
+        int coinCount;
+        int num=0; // 錢index
+        for (int j=3; j>=0 && n>0; j--) { // large to small
+            coinCount = n/coin[j]; // num of coin[j]
+            printf("%d元%d個 ",coin[j],coinCount);
+            num += coinCount;
+            n %= coin[j]; // remaining 餘數
+        }
+        printf("共%d個\n",num);
+    }
+    return 0;
+}
